f0cc3212731c20cfb272124852d24379630f88c1
Neil Jaraula <neiljaraula@gmail.com>
2024-10-17 18:22:08 +0800

    deploy na

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
 const MongoStore = require("connect-mongo");
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 // -----atlas connect-----
 // process.env.DB_UR
 // -----local connect-----
@@ -47,12 +47,13 @@ app.use(
     replaceWith: "_",
   })
 );
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
   crypto: {
-    secret: "thisshouldbeabettersecret!",
+    secret,
   },
 });
 
@@ -63,7 +64,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -153,4 +154,5 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => [console.log("Serving on port 3000")]);
+const port = process.env.PORT || 3000;
+app.listen(port, () => [console.log("Serving on port 3000")]);
